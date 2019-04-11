9661d7b99df6785de65121c6a18efb45afd41795
Shadowsith <shadowsith94@gmail.com>
2019-04-12 00:14:40 +0200

    editing install script to get rvm correctly

--- install.sh
+++ install.sh
@@ -13,12 +13,11 @@ mkdir ~/temp
 mkdir -p ~/mpd1/playlists
 
 echo Install and set up ruby and all needed libraries
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -L https://get.rvm.io | bash -s stable
+gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable --ruby
 source ~/.rvm/scripts/rvm
 rvm autolibs disable
 rvm requirements
-rvm install ruby --latest
 rvm --create use @bots
 
 echo Get and build mumble-ruby and ruby-mpd and other dependencies
