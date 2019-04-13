ff9bfcd397aae810bda6694041ee0af9d84fa783
Shadowsith <shadowsith94@gmail.com>
2019-04-14 01:19:37 +0200

    adding new sites

--- website/rb/YmlTemplate.rb
+++ website/rb/YmlTemplate.rb
@@ -10,36 +10,131 @@ class YmlTemplate
   @@mixcloud_config = @path + "/../../plugins/mixcloud.yml"
   @@ektoplazm_config = @path + "/../../plugins/ektoplazm.yml"
   @@bandcamp_config = @path + "/../../plugins/bandcamp.yml"
+  @@soundcloud_config = @path + "/../../plugins/soundcloud.yml"
   @@googletts_config = @path + "/../../plugins/googletts.yml"
   @@picotts__config = @path + "/../../plugins/picotts.yml"
 
-  def setupValuesForWeb
-    if @yt['to_mp3'] == true
-      @yt['to_mp3'] = 1
-    else 
-      @yt['to_mp3'] = 0
-    end
-  end
-
   public
+  attr_reader :mpd, :yt, :yt_dl, :sc, :bc, :mc, :ep
+  attr_reader :googletts, :picotts
+  attr_writer :yt_dl
+
   def initialize
+    puts 'hi'
     #load yaml
-    @mpd = YAML.load_file(@@mpd_config)
+    @mpd_file = YAML.load_file(@@mpd_config)
     @youtube = YAML.load_file(@@youtube_config)
     @idle = YAML.load_file(@@idle_config)
     @mixcloud = YAML.load_file(@@mixcloud_config)
     @ektoplazm = YAML.load_file(@@ektoplazm_config)
     @bandcamp = YAML.load_file(@@bandcamp_config)
+    @soundcloud = YAML.load_file(@@soundcloud_config)
     @googletts = YAML.load_file(@@googletts_config)
     @picotts = YAML.load_file(@@picotts__config)
 
     #for simpler using it in haml
+    @mpd = @mpd_file['plugin']['mpd']
     @yt = @youtube['plugin']['youtube']
-    @yt_dl = @yt['youtube_dl']
-
-    setupValuesForWeb()
+    @sc = @soundcloud['plugin']['soundcloud']
+    @bc = @bandcamp['plugin']['bandcamp']
+    @mc = @mixcloud['plugin']['mixcloud'] 
+    @ep = @ektoplazm['plugin']['ektoplazm']
+    @yt_dl = nil # is set on get
   end
 
-  attr_reader :mpd, :yt, :yt_dl, :idle, :mixcloud, :ektoplazm
-  attr_reader :bandcamp, :googletts, :picotts
+  def writeYtDl(params) 
+    @yt_dl['path'] = params['ytdl_path']
+    @yt_dl['options'] = params['ytdl_options']
+    @yt_dl['prefixes'] = params['ytdl_prefixes']
+  end
+
+  def writeBack(yml, params)
+    yml['youtube_dl'] = @yt_dl
+
+    yml['folder']['download'] = params['folder_download']
+    yml['folder']['temp'] = params['folder_temp']
+    if params['to_mp3'] == '1'
+      yml['to_mp3'] = true
+    else
+      yml['to_mp3'] = false
+    end
+  end
+
+  def saveYoutube(params)
+    writeYtDl(params)
+    @yt_dl['maxresults'] = params['ytdl_maxresults']
+    @yt['youtube_dl'] = @yt_dl
+
+    @yt['folder']['download'] = params['folder_download']
+    @yt['folder']['temp'] = params['folder_temp']
+    @yt['stream'] = params['stream']
+    if params['to_mp3'] == 1
+      @yt['to_mp3'] == true
+    else
+      @yt['to_mp3'] == false
+    end
+
+    @youtube['plugin']['youtube'] = @yt
+    File.open(@@youtube_config, 'w') {|f| f.write @youtube.to_yaml}
+  end
+
+  def saveMpd(post)
+    @mpd['testpipe'] = post['testpipe']
+    @mpd['volume'] = post['volume']
+    @mpd['host'] = post['host']
+    @mpd['port'] = post['port']
+    @mpd['musicfolder'] = post['musicfolder']
+    @mpd['template']['comment']['enabled'] = post['enabled']
+    @mpd['template']['comment']['disabled'] = post['disabled']
+    
+    @mpd_file['plugin']['mpd'] = @mpd
+    File.open(@@mpd_config, 'w') {|f| f.write @mpd_file.to_yaml} 
+  end
+
+  def saveSoundCloud(post)
+    writeYtDl(post)
+    writeBack(@sc, post)
+    @soundcloud['plugin']['soundcloud'] = @sc
+    File.open(@@soundcloud_config, 'w') {|f| f.write @soundcloud.to_yaml} 
+  end
+
+  def saveMixCloud(post)
+    writeYtDl(post)
+    writeBack(@mc, post)
+    @mixcloud['plugin']['mixcloud'] = @mc
+    File.open(@@mixcloud_config, 'w') {|f| f.write @mixcloud.to_yaml} 
+  end
+
+  def saveMixCloud(post)
+    writeYtDl(post)
+    writeBack(@mc, post)
+    @mixcloud['plugin']['mixcloud'] = @mc
+    File.open(@@mixcloud_config, 'w') {|f| f.write @mixcloud.to_yaml} 
+  end
+
+  def saveBandCamp(post)
+    writeYtDl(post)
+    writeBack(@bc, post)
+    @bandcamp['plugin']['bandcamp'] = @bc
+    File.open(@@bandcamp_config, 'w') {|f| f.write @bandcamp.to_yaml} 
+  end
+
+  def saveEktoplazm(post)
+    @ep['folder']['download'] = params['folder_download']
+    @ep['folder']['temp'] = params['folder_temp']
+    @ep['prefixes'] = params['prefixes']
+
+    @ektoplazm['plugin']['ektoplazm'] = @ep
+    File.open(@@ektoplazm_config, 'w') {|f| f.write @ektoplazm.to_yaml} 
+  end
+
+  def saveGoogleTTS(post)
+    @googletts['lang'] = post['lang']
+    File.open(@@googletts_config, 'w') {|f| f.write @googletts.to_yaml} 
+  end
+
+  def savePicoTTS(post)
+    @picotts['lang'] = post['lang']
+    File.open(@@picotts_config, 'w') {|f| f.write @picotts.to_yaml} 
+  end
 end
--- website/website.rb
+++ website/website.rb
@@ -11,12 +11,14 @@ class Pluginbot < Sinatra::Base
 
   public
   set :public_folder, File.dirname(File.expand_path(__FILE__)) + '/'
+  enable :sessions
   def login(params)
     usr = params['username']
     pwd = params['password']
     # dummy test 'login'
     if (usr == 'hello' && pwd == 'world')
-      haml :index
+      session['login'] = true
+      redirect '/index'
     else 
       haml :login
     end 
@@ -29,36 +31,97 @@ class Pluginbot < Sinatra::Base
   def yml
     return @@yml
   end
+
+  def setRoute(route) 
+    if(route != 'login') 
+      redirect '/login' if session['login'] != true
+      @index_content = route
+    end
+
+    case route
+      when 'login'
+        redirect '/index' if session['login'] == true
+        haml :login
+      when 'index'
+        @index_content = 'global'
+        haml :index
+      when 'youtube'
+        @@yml.yt_dl = @@yml.yt['youtube_dl']
+        haml :index
+      when 'mpd'
+        haml :index
+      when 'ektoplazm' 
+        haml :index
+      when 'bandcamp'
+        @@yml.yt_dl = @@yml.bc['youtube_dl']
+        haml :index 
+      when 'mixcloud'
+        @@yml.yt_dl = @@yml.mc['youtube_dl']
+        haml :index
+      when 'soundcloud'
+        @@yml.yt_dl = @@yml.sc['youtube_dl']
+        haml :index 
+      when 'idle'
+        haml :index
+      when 'googletts'
+        haml :index
+      when 'picotts'
+        haml :index
+      when '/logout'
+        session['login'] = false
+        redirect '/login'
+      else 
+        haml :login
+    end
+  end
   
   # Endpoints
-  get '/' do
-    haml :login
-  end
-
-  get '/index' do
-    @index_content = 'global'
-    haml :index 
-  end
-
-  get '/youtube' do 
-    @index_content = 'youtube'
-    haml :index
-  end
-
-  get '/login' do
-    haml :login
-  end
-
-  post '/' do 
-    login(params)
+  
+  get '/*' do
+    route = params[:splat].first
+    setRoute(route)
   end
 
   post '/login' do
     login(params)
   end
 
-  get '/help' do
-    haml :help
+  post '/youtube' do 
+    @@yml.saveYoutube(params)
+    redirect '/youtube'
+  end
+
+  post '/mpd' do 
+    @@yml.saveMpd(params)
+    redirect '/mpd'
+  end
+
+  post '/soundcloud' do 
+    @@yml.saveSoundCloud(params)
+    redirect '/soundcloud'
+  end
+
+  post '/mixcloud' do 
+    @@yml.saveMixCloud(params)
+    redirect '/mixcloud'
+  end
+
+  post '/bandcamp' do 
+    @@yml.saveBandCamp(params)
+    redirect '/bandcamp'
+  end
+
+  post '/ektoplazm' do 
+    @@yml.saveEktoplazm(params)
+    redirect '/ektoplazm'
+  end
+
+  post '/googletts' do 
+    redirect '/googletts'
+  end 
+
+  post '/picotts' do
+    redirect '/picotts'
   end
 
   run!
